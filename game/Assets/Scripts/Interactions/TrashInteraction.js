d4fe7a384280ee9bf8ffede5d23b5fc5a39bc1e5
Wolfsie <ajmuir.fgcu@gmail.com>
2014-07-28 16:15:22 -0400

    Added Scripts
    Added scripts to a few objects in sandbox (ricky's mesh and the
    trashcans). Also added a few folders to organize scripts.

---
+++ game/Assets/Scripts/Interactions/TrashInteraction.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+// public class = TrashInteraction
+// Debug.Log(TrashInteraction.transform.position.x);
+// public var trashOpen = false;
+// public var trashPositionX = gameObject.transform.position.x;
+// public var trashPositionZ = gameObject.transform.position.z;
+// public var rickyScript : rickyScript;
+// private var boxCol :BoxCollider;
+
+
+// function Update () {
+// 	//Check Ricky's position. If it is within the bounds of the x and z
+// 	//coordinates of the trash then trigger this function
+// 	boxCol = rickyScript.GetComponent(BoxCollider);
+// 	if(ricky.position.x >= 2 &&
+// 	   ricky.position.x <= 4 && 
+// 	   ricky.position.z >= 2 &&
+// 	   ricky.position.z <= 4 ) {
+
+// 		Debug.Log("at can");
+// 	}
+// 	// Debug.Log(trashPositionZ);
+
+// }
