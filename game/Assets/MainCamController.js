de3d916f7181ad24d6d061a9d64815d8e543b138
JELL K <jayk@tunepusher.com>
2014-07-31 18:39:20 -0400

    EVERYONE LOOK!! HE WALKS!!! (Hold yo mouse down foo!)

---
+++ game/Assets/MainCamController.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+ 
+///    
+///    SmoothFollow:
+///        Follows the target's position while applying damping to prevent jittering
+///        and immediate moving of the camera.This way every subtle movement doesn't become
+///        noticeable. The camera follows from a set distance away from the character and always
+///        tries to center the view on the target.
+///
+ 
+// public fields
+var target : Transform;                    // the target camera is following
+var dampTime : float = 0.3;             // offset from the viewport center to fix damping
+var distance : float = 10;              // distance away on z axis from the target
+ 
+// private fields
+private var _velocity = Vector3.zero;   // velocity of camera relative to target
+ 
+/// 
+///    Update:
+///        Makes sure that the camera is always focused on the target, 
+///        after applying damping.
+///
+function Update() {
+	target = gameObject.Find("Ricky").GetComponent("Transform");
+    if(target) {
+        // calculate the difference between camera and target positions
+        var delta : Vector3 = target.position - camera.ViewportToWorldPoint(Vector3(0.5, 0.5, distance));
+ 
+        // calculate destination of the camera based on the difference in positions
+        var destination : Vector3 = transform.position + delta;
+ 
+        // apply the new position through dampening to provide smooth movement
+        transform.position = Vector3.SmoothDamp(transform.position, destination, _velocity, dampTime);
+    }
+} // eo Update
--- game/Assets/RickyMovements.js
+++ game/Assets/RickyMovements.js
@@ -3,56 +3,81 @@
 var walkSpeed: int = 5;
 var jumpHeight: int = 10;
 
-var spawn:GameObject;
+//var spawn:GameObject;
 
 
 function Start () {
-	transform.position = spawn.transform.position;
+//	transform.position = spawn.transform.position;
 
 }
 
+// Click To Move script
+// Moves the object towards the mouse position on left mouse click
+ 
+var smooth:int; // Determines how quickly object moves towards position
+ 
+private var targetPosition:Vector3;
+ 
 function Update () {
-
-}
-
-function FixedUpdate () {
-	rigidbody.velocity.x = walkSpeed;
-
-	var startOfRay = transform.position;
-	startOfRay.y -= 1.4;
-
-	// Debug.DrawRay(startOfRay, -Vector2.up, Color.green, 1);
-
-	var hit:RaycastHit2D = Physics.Raycast(startOfRay, -Vector2.up, 0.01);
-	var animatorComponent:Animator = this.GetComponent("Animator");
-
-
-
-	if (hit.collider) //Mario is on the ground
+	if(Input.GetKey(KeyCode.Mouse0))
 	{
-		
+		var playerPlane = new Plane(Vector3.up, transform.position);
+		var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		var hitdist = 0.0;
 
-		if ( Input.GetAxis("Vertical") )
-		{	
-			rigidbody.velocity.y = jumpHeight;
-			animatorComponent.SetInteger("state", 1);
+		
+// 		rigidbody.velocity.x = walkSpeed;
+
+		if (playerPlane.Raycast (ray, hitdist)) {
+			var targetPoint = ray.GetPoint(hitdist);
+			targetPosition = ray.GetPoint(hitdist);
+			var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+			transform.rotation = targetRotation;
+			transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * .5);
+//			Debug.DrawRay(transform.position, targetPosition.forward, Color.green, 1);
+
 
 		}
-
-	} else //Mario is in the air
-	{
-		animatorComponent.SetInteger("state", 0);
 	}
-
-
-	if ( transform.position.y < -6 ){ //Fell below world
-		Debug.Log("Mario is Dead. Long live the Mario.");
-		// transform.position.x = spawn.transform.position.x;
-		// transform.position.y = spawn.transform.position.y;
-		transform.position = spawn.transform.position;
-	}
-
-
-	
-
 }
+
+//function FixedUpdate () {
+//
+//	var startOfRay = transform.position;
+//	startOfRay.y -= 1.4;
+//
+//	// Debug.DrawRay(startOfRay, -Vector2.up, Color.green, 1);
+//
+//	var hit:RaycastHit = Physics.Raycast(startOfRay, -Vector2.up, 0.01);
+//	var animatorComponent:Animator = this.GetComponent("Animator");
+//
+//
+//
+//	if (hit.collider) //Mario is on the ground
+//	{
+//		
+//
+//		if ( Input.GetAxis("Vertical") )
+//		{	
+//			rigidbody.velocity.y = jumpHeight;
+//			animatorComponent.SetInteger("state", 1);
+//
+//		}
+//
+//	} else //Mario is in the air
+//	{
+//		animatorComponent.SetInteger("state", 0);
+//	}
+//
+//
+//	if ( transform.position.y < -6 ){ //Fell below world
+//		Debug.Log("Mario is Dead. Long live the Mario.");
+//		// transform.position.x = spawn.transform.position.x;
+//		// transform.position.y = spawn.transform.position.y;
+//		transform.position = spawn.transform.position;
+//	}
+//
+//
+//	
+//
+//}
