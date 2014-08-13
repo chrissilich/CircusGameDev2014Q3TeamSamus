171906e94643c559918d79f801d0cabe3db29001
Amanda Muir <muir.dev@gmail.com>
2014-08-13 16:24:07 -0400

    Prefabs, Particles, and Interactions
    Made fog, rain, and fireflies.
    Coded cache interactions (except for buttons).
    Coded porch light and light's interaction with trash.
    Cleared out original test art (we should clear out unused and test art
    when lvl 1 is finished and we leave the sandbox. It'll cut down on our
    file size a LOT)
    Changed the names of a few things
    Created Prefabs out of as much of the sandbox as possible for switching
    them over to Lvl 1

---
+++ game/Assets/Scripts/Interactions/CacheController.js
@@ -0,0 +1,50 @@
+#pragma strict
+private var cache:GameObject;
+function Start () {
+	cache = gameObject.Find("CacheModal");
+	cache.SetActive(false);
+
+}
+
+
+
+function Update () {
+
+}
+
+
+private var cacheTriggered:boolean = false;
+private var timeSinceCache:int = 0;
+function OnTriggerEnter() {
+	if(!cacheTriggered) {
+		Debug.Log("Cache");
+		cache.SetActive(true);
+	} else {
+		//do nothing, it hasn't been 10 secs since they left, 
+		//so this is probably not a trip to actually drop off trash,
+		//the player probably just accidentally went back into the
+		//collider and doesn't actually want to see the cache modal.
+	}
+}
+function OnTriggerExit() {
+	if(!cacheTriggered) {
+		cacheTriggered = true;
+		InvokeRepeating("cacheTimer", 0.1, 1);
+	} else {
+		//do nothing, it hasn't been 10 secs since they left, 
+		//so this is probably not a trip to actually drop off trash,
+		//the player probably just accidentally went back into the
+		//collider and doesn't actually want to see the cache modal.
+	}
+}
+
+function cacheTimer () {
+	if(timeSinceCache <=8) {
+		timeSinceCache++;
+	} else{
+		timeSinceCache = 0;
+		cacheTriggered = false;
+		CancelInvoke("cacheTimer");
+	}
+}
+
