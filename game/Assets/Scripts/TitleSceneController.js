fd8f5aeaa086a0561b52b027ddafba1ebe1d5da0
Wolfsie <ajmuir.fgcu@gmail.com>
2014-08-14 10:00:34 -0400

    Updating GUI and Title Screen

--- game/Assets/Scripts/TitleSceneController.js
+++ game/Assets/Scripts/TitleSceneController.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 
 var playButtonTexture:Texture;
+var sandButtonTexture:Texture;
 function OnGUI() {
 	//Button is a method of GUI
 	//It can't just recieve ints, it must recieve a rectangle, dimensions, and text, and styles
@@ -9,6 +10,14 @@ function OnGUI() {
 
 	//The way you check to see if somehting has been clicked is truthy or falsey (so a boolean)
 	if(playButton){
+		//Switch scenes with the Global Function Application
+		//Use the name of the LVL, not the Index, because if you put in a new LVL that index may change
+		Application.LoadLevel("ColoredLevel1");
+	}
+	var sandButton = GUI.Button(Rect(140, 600, 200, 100), sandButtonTexture, GUIStyle.none);
+
+	//The way you check to see if somehting has been clicked is truthy or falsey (so a boolean)
+	if(sandButton){
 		//Switch scenes with the Global Function Application
 		//Use the name of the LVL, not the Index, because if you put in a new LVL that index may change
 		Application.LoadLevel("SandBox Lvl1");
