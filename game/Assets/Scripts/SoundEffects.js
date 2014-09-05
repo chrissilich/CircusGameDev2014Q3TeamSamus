cf143499f670ad8c1e40f2e874c3eb428f94897a
JELL K <jayk@tunepusher.com>
2014-09-05 00:37:15 -0400

    Walking sound and colliders

---
+++ game/Assets/Scripts/SoundEffects.js
@@ -0,0 +1,16 @@
+﻿var sound : AudioClip;
+
+function Start(){
+	audio.Pause();
+    audio.clip = sound;
+}
+ 
+function Update (){    
+    if(Input.GetMouseButtonDown(0)){
+    Debug.Log("MOUSE DOWNNNNN.");
+        audio.Play();
+    }if(Input.GetMouseButtonUp(0)){
+    Debug.Log("MOUSE UPPPPPPP.");
+        audio.Stop();
+    }
+}
