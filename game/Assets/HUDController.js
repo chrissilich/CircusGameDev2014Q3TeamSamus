336bc81ba83eaf551b54b46edd9e5d3cb7624b68
Wolfsie <ajmuir.fgcu@gmail.com>
2014-09-07 02:10:50 -0400

    Some Menu stuff

--- game/Assets/HUDController.js
+++ game/Assets/HUDController.js
@@ -1,9 +1,7 @@
 ﻿#pragma strict
 var LevelController:LevelController;
-var CacheController:CacheController;
 function Start () {
 	LevelController = gameObject.Find("LevelController").GetComponent("LevelController");
-	CacheController = gameObject.Find("Cache").GetComponent("CacheController");
 }
 
 function Update () {
@@ -18,34 +16,47 @@ function OnGUI () {
     var halfX = (Camera.main.pixelWidth/2)-300;
     var halfY = (Camera.main.pixelHeight/2)-200;
     GUI.Box (Rect (halfX,halfY,600,400), "Would You Like To Store Your Trash or Pick Up Your Stash?", customBG);
+    GUI.Label (Rect(halfX, halfY+105, 600, 100), "score "+LevelController.cachedTrash+" pieces of trash.", customLabel);
     GUI.Label (Rect(halfX, halfY+105, 600, 100), "Your cache currently has "+LevelController.cachedTrash+" pieces of trash.", customLabel);
-    // Make the first button. 
-    if (GUI.Button (Rect ((Camera.main.pixelWidth/2)-250,(Camera.main.pixelHeight/2.5)+35,200,100), "Store Trash", customBtn)) {
-        //Dump trash
-        //Set CachedTrash to equal CachedTrash+CurrentTrash
-        //Set currentTrash to 0
-        //Close Modal
-        LevelController.storeTrash();
-        LevelController.currentTrash = 0;
-        CacheController.closeCacheModal();
-    }
+    GUI.Label (Rect(halfX, halfY+105, 600, 100), "Awareness "+LevelController.cachedTrash+" pieces of trash.", customLabel);
+    GUI.Label (Rect(halfX, halfY+105, 600, 100), "Awareness "+LevelController.cachedTrash+" pieces of trash.", customLabel);
 
-    // Make the second button.
-    if (GUI.Button (Rect ((Camera.main.pixelWidth/2)+50,(Camera.main.pixelHeight/2.5)+35,200,100), "Pick Up Stash", customBtn)) {
-        //PickUp trash
-        //Set CurrentTrash to equal CachedTrash+CurrentTrash
-        //Set CachedTrash to 0
-        //Close Modal
-        LevelController.pickUpTrash();
-        LevelController.cachedTrash = 0;
-        CacheController.closeCacheModal();
-        Debug.Log("cachedTrash = " + LevelController.cachedTrash);
-    }
+}
 
-    //Make the third button
-    if (GUI.Button (Rect ((Camera.main.pixelWidth/2)-75,(Camera.main.pixelHeight/2.5)+35+125,150,75), "Close", customBtn)) {
-        //User doesn't want to do either
-        //Close Modal
-        CacheController.closeCacheModal();
-    }
+
+
+//SCORE CONTROLLER
+#pragma strict
+var LevelController:LevelController;
+
+function Start() {
+	LevelController = gameObject.Find("LevelController").GetComponent("LevelController");
+}
+
+
+function OnGUI () {
+	guiText.text = LevelController.currentTrash.ToString();
+}
+
+function Update () {
+
+}
+
+
+
+//AWARE CONTROLLER
+#pragma strict
+var LevelController:LevelController;
+
+function Start() {
+	LevelController = gameObject.Find("LevelController").GetComponent("LevelController");
+}
+
+
+function OnGUI () {
+	guiText.text = LevelController.currentAware.ToString();
+}
+
+function Update () {
+
 }
